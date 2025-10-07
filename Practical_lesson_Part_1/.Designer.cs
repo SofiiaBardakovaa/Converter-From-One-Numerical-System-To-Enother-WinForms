95b80a0464a6ec8a02256acda820ecfdfcc175af
Софія Бардакова <sofiia.bardakova@nure.ua>
2025-10-07 14:36:40 +0300

    Add project files.

---
+++ Practical_lesson_Part_1/.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Practical_lesson_Part_1
+{
+    partial class ConverterFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.comboFrom = new System.Windows.Forms.ComboBox();
+            this.comboTo = new System.Windows.Forms.ComboBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(354, 23);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(308, 26);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.LavenderBlush;
+            this.txtResult.Location = new System.Drawing.Point(189, 226);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(447, 26);
+            this.txtResult.TabIndex = 1;
+            // 
+            // comboFrom
+            // 
+            this.comboFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboFrom.FormattingEnabled = true;
+            this.comboFrom.Items.AddRange(new object[] {
+            "2",
+            "8",
+            "16"});
+            this.comboFrom.Location = new System.Drawing.Point(169, 70);
+            this.comboFrom.Name = "comboFrom";
+            this.comboFrom.Size = new System.Drawing.Size(141, 28);
+            this.comboFrom.TabIndex = 2;
+            this.comboFrom.SelectedIndexChanged += new System.EventHandler(this.comboFrom_SelectedIndexChanged);
+            // 
+            // comboTo
+            // 
+            this.comboTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboTo.FormattingEnabled = true;
+            this.comboTo.Items.AddRange(new object[] {
+            "2",
+            "8",
+            "16"});
+            this.comboTo.Location = new System.Drawing.Point(518, 70);
+            this.comboTo.Name = "comboTo";
+            this.comboTo.Size = new System.Drawing.Size(144, 28);
+            this.comboTo.TabIndex = 3;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnConvert.Location = new System.Drawing.Point(251, 132);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(193, 62);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Перетворити";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblInput.Location = new System.Drawing.Point(53, 22);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(257, 25);
+            this.lblInput.TabIndex = 5;
+            this.lblInput.Text = "Введіть число (2, 8, 16):";
+            this.lblInput.Click += new System.EventHandler(this.lblFrom_Click);
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTo.Location = new System.Drawing.Point(397, 70);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(101, 24);
+            this.lblTo.TabIndex = 6;
+            this.lblTo.Text = "У систему:";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblFrom.Location = new System.Drawing.Point(61, 70);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(102, 24);
+            this.lblFrom.TabIndex = 7;
+            this.lblFrom.Text = "З системи:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(53, 224);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(121, 27);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "Результат:";
+            // 
+            // ConverterFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(767, 322);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.comboTo);
+            this.Controls.Add(this.comboFrom);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Name = "ConverterFrom";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Конвертер систем числення";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ComboBox comboFrom;
+        private System.Windows.Forms.ComboBox comboTo;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Practical_lesson_Part_1/Converter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical_lesson_Part_1
+{
+    public partial class ConverterFrom : Form
+    {
+        public ConverterFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void lblFrom_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboFrom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string input = txtInput.Text.Trim().ToUpper();
+            string result = "";
+            string fromSystem = comboFrom.SelectedItem.ToString();
+            string toSystem = comboTo.SelectedItem.ToString();
+
+            if (fromSystem == null || toSystem == null)
+            {
+                MessageBox.Show("Оберіть системи числення!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                int fromBase = int.Parse(fromSystem);
+                int toBase = int.Parse(toSystem);
+
+                int decimalValue = Convert.ToInt32(input, fromBase);
+
+                if (toBase == 2)
+                    result = Convert.ToString(decimalValue, 2);
+                else if (toBase == 8)
+                    result = Convert.ToString(decimalValue, 8);
+                else if (toBase == 16)
+                    result = Convert.ToString(decimalValue, 16).ToUpper();
+                else
+                    result = "Невідома система";
+            }
+            catch
+            {
+                MessageBox.Show("Некоректне число для обраної системи!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            txtResult.Text = result;
+        }
+    }
+}
